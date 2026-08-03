07810530f9a64b48837d072e49805c729a36bfb7
Gabriel Takaoka <90276542+GabrielTakaoka@users.noreply.github.com>
2026-08-03 20:58:42 -0300

    Add files via upload

--- EngineeringFiap2/listas.py
+++ EngineeringFiap2/listas.py
@@ -23,11 +23,27 @@ compras += ["Roda", "Fuso trapezoidal"]
 compras.extend(["Pablo", "Boiola"])
 print(compras)'''
 
-lista1 = ["Pepino", "Brócolis", "Cenoura", "Caju"]
+'''lista1 = ["Pepino", "Brócolis", "Cenoura", "Caju"]
 lista2 = ["Geléia", "Maionese", "Manteiga", "Mostarda", "Katchup"]
 
 lista3 = lista1 + lista2
 
-print(lista1)
-print(lista2)
-print(lista3)
+opcao = str(input("Digite o produto desejado: "))
+for i in lista3:
+    if opcao == i:
+        print(f'O produto {i} foi encontrado!')
+        break
+else:
+    print("Produto não encontrado")'''
+
+'''lista1 = [4, 68, 10, 2, 31, 24, 1]
+
+minimo = lista1[0]
+
+for i in lista1:
+    if minimo > i:
+        minimo = i
+
+print(minimo)'''
+
+listaT = [-10, -8, 0, ]
