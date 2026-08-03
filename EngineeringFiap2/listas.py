3eff63e1ba34b201458cfcd130b23acbb91a13aa
Gabriel Takaoka <90276542+GabrielTakaoka@users.noreply.github.com>
2026-08-03 20:31:36 -0300

    Add files via upload

---
+++ EngineeringFiap2/listas.py
@@ -0,0 +1,33 @@
+'''compras = ["Arroz", "Feijão", "Frango", "Batata"]
+
+compras_novo = compras.copy()
+
+print(compras_novo)
+
+compras.append("Nutella")
+compras_novo.append("Salada")
+
+print(compras_novo)
+
+print(compras[0:2])
+
+n_compras = len(compras)
+
+print(n_compras)
+
+print(compras_novo[1])'''
+
+'''compras = []
+compras += ["Polia"]
+compras += ["Roda", "Fuso trapezoidal"]
+compras.extend(["Pablo", "Boiola"])
+print(compras)'''
+
+lista1 = ["Pepino", "Brócolis", "Cenoura", "Caju"]
+lista2 = ["Geléia", "Maionese", "Manteiga", "Mostarda", "Katchup"]
+
+lista3 = lista1 + lista2
+
+print(lista1)
+print(lista2)
+print(lista3)
